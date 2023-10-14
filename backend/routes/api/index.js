688aa5f57416d7bccf0797f39e75dcc06c039443
Brian <brian_@DESKTOP-GJH2BCP>
2023-10-14 18:54:12 -0400

    login post route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
+const sessionRouter = require('./session');
+const usersRouter = require('./users')
 
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 router.use(restoreUser)
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
 router.get(
   '/restore-user',
