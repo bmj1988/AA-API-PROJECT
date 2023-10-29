8fe631e2e326aa9a37bfbf114f22fa8619918065
Brian <cjcores@gmail.com>
2023-10-29 02:18:17 -0400

    removing migration to add firstName, lastName, added manually into main migration

--- backend/db/migrations/1-Create-User.js
+++ backend/db/migrations/1-Create-User.js
@@ -26,6 +26,14 @@ module.exports = {
       hashedPassword: {
         type: Sequelize.STRING
       },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
