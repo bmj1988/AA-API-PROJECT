14986c469f88f6ff6f584dc0a2a043bd2f43a451
Brian <cjcores@gmail.com>
2023-12-13 22:29:42 -0500

    single quotes

--- backend/db/migrations/20231029075546-add-previewImage-to-spot.js
+++ backend/db/migrations/20231029075546-add-previewImage-to-spot.js
@@ -3,7 +3,7 @@ let options = {};
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 }
-options.tableName = "Spots";
+options.tableName = 'Spots';
 
 module.exports = {
   async up (queryInterface, Sequelize) {
