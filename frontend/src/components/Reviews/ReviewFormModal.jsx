60cb84a32c074c5b923d66fcd70031d35438359f
Brian <cjcores@gmail.com>
2023-12-19 23:04:45 -0500

    fixed classname again

--- frontend/src/components/Reviews/ReviewFormModal.jsx
+++ frontend/src/components/Reviews/ReviewFormModal.jsx
@@ -57,7 +57,7 @@ const ReviewFormModal = ({ spotId, userId }) => {
                         <i className="fa-solid fa-star"></i>
                     </div>
                 </div>
-                <textarea rows='8' className="textmark" maxLength={500} placeholder="Leave your review here" autoFocus onChange={(e) => setReview(e.target.value)} />
+                <textarea rows='8' className="textmark reviewTexta" maxLength={500} placeholder="Leave your review here" autoFocus onChange={(e) => setReview(e.target.value)} />
                 <button disabled={review.length < 10 || rating < 1}>Submit Your Review</button>
             </form>
         </div>
